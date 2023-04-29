48228d2ea57910ed5fbe9699b65e692a5bb8a67c
abdelrahmanyasser65 <abdelrahman117ma@gmail.com>
2023-04-29 08:44:50 +0300

    chat_state

---
+++ lib/features/chat/presentation/view_model/chat_state.dart
@@ -0,0 +1,12 @@
+part of 'chat_cubit.dart';
+
+@immutable
+abstract class ChatState {}
+
+class ChatInitialState extends ChatState {}
+class ChatSuccessState extends ChatState {}
+class ChatLoadingState extends ChatState {}
+class ChatErrorState extends ChatState {
+  final String errorMessage;
+   ChatErrorState(this.errorMessage);
+}
